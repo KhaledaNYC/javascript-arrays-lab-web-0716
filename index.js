2c55e7f68d298e84c4aa9b9fc52519d5187e5ceb
KhaledaNYC <khaleda.khan87@gmail.com>
2016-08-30 14:37:32 -0400

    destructive array methods

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const app = "I don't do much."
+
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+var newKittens = kittens;
+var destructivelyAppendKitten = function(name){
+  // kittens = ["Milo", "Otis", "Garfield"];
+  kittens.push(name);
+};
+
+var destructivelyPrependKitten = function (name) {
+  // kittens = ["Milo", "Otis", "Garfield"];
+  kittens.unshift(name);
+};
+
+var destructivelyRemoveLastKitten = function () {
+  // kittens = ["Milo", "Otis", "Garfield"];
+  kittens.pop();
+};
+var destructivelyRemoveFirstKitten = function () {
+  // kittens = ["Milo", "Otis", "Garfield"];
+  kittens.shift();
+};
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var appendKitten = function(name){
+    var kittens = ["Milo", "Otis", "Garfield"];
+    var newKittens = kittens;
+    newKittens.push(name);
+    return newKittens;
+};
+
+var prependKitten = function (name) {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  var newKittens = kittens;
+  newKittens.unshift(name);
+  return newKittens;
+};
+
+var removeLastKitten = function () {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  var newKittens = kittens;
+  newKittens.pop();
+  return newKittens;
+};
+var removeFirstKitten = function () {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  var newKittens = kittens;
+  newKittens.shift();
+  return newKittens;
+};
